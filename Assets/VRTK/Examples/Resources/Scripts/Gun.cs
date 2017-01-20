cea0c01c928cb2137e8f339e8f7f10c3cc1ba81b
tyfurness <tyfurness@gmail.com>
2017-01-20 13:25:42 -0500

    stuff
    More updates.

--- Assets/VRTK/Examples/Resources/Scripts/Gun.cs
+++ Assets/VRTK/Examples/Resources/Scripts/Gun.cs
@@ -1,5 +1,6 @@
 ﻿namespace VRTK.Examples
 {
+    using System.Collections;
     using UnityEngine;
 
     public class Gun : VRTK_InteractableObject
@@ -8,10 +9,14 @@
         private float bulletSpeed = 2000f;
         private float bulletLife = 5f;
 
+        [SerializeField]
+        private Animator gunShot;
+
         public override void StartUsing(GameObject usingObject)
         {
             base.StartUsing(usingObject);
-            FireBullet();
+            gunShot.SetTrigger("hasShot");
+            Fire();
         }
 
         protected void Start()
@@ -20,13 +25,19 @@
             bullet.SetActive(false);
         }
 
-        private void FireBullet()
+        private void Fire()
         {
             GameObject bulletClone = Instantiate(bullet, bullet.transform.position, bullet.transform.rotation) as GameObject;
             bulletClone.SetActive(true);
             Rigidbody rb = bulletClone.GetComponent<Rigidbody>();
             rb.AddForce(-bullet.transform.forward * bulletSpeed);
+
             Destroy(bulletClone, bulletLife);
+        }
+
+        public void ANIM_ONSHOTCOMPLETE()
+        {
+            gunShot.ResetTrigger("hasShot");
         }
     }
 }
